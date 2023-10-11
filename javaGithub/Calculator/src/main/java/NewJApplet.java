eeb04e0b4afee69ffdee27118919153d59f37537
Priyanshu Sharma <101499662+priyanshusharma44@users.noreply.github.com>
2023-10-11 22:17:14 +0545

    Add files via upload

---
+++ javaGithub/Calculator/src/main/java/NewJApplet.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/JApplet.java to edit this template
+ */
+
+import javax.swing.JApplet;
+
+/**
+ *
+ * @author DarkNull
+ */
+public class NewJApplet extends JApplet {
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    public void init() {
+        // TODO start asynchronous download of heavy resources
+    }
+
+    // TODO overwrite start(), stop() and destroy() methods
+}
